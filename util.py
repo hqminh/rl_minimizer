be0b3e9878c43c1339f5de6af5a79c68664c7756
hqminh <50564971+hqminh@users.noreply.github.com>
2021-04-24 12:05:06 -0400

    Add files via upload

--- util.py
+++ util.py
@@ -4,7 +4,9 @@ import torch.nn as nn
 from copy import deepcopy
 from torch.optim import Adam
 from collections import deque
-
+from pprint import pprint
+import subprocess
+import sys, os
 
 # Return 1 by vocab_size
 def onehot(t, vocab_size=4):
@@ -52,3 +54,24 @@ def windows_to_multihot(windows, w, k, vocab_size=4):
         v.append(kmers_to_multihot(kmer, vocab_size))
 
     return torch.stack(v).transpose(0, 1)
+
+
+def state_dict_to_tensor(state_dict):
+    return torch.cat([t.view(-1) for t in state_dict.values()])
+
+
+def tensor_to_state_dict(w, state_dict):
+    w = w.squeeze()
+    curr = 0
+    for k in state_dict:
+        tsize = torch.numel(state_dict[k])
+        state_dict[k] = w[curr: curr + tsize].reshape(state_dict[k].shape)
+        curr += tsize
+
+def ensure_not_1D(x):
+    if x.ndim == 1:
+        if isinstance(x, np.ndarray):
+            x = torch.tensor(np.expand_dims(x, axis=0))
+        elif isinstance(x, torch.Tensor):
+            x = x.unsqueeze(0)
+    return x
